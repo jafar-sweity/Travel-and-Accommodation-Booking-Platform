a72a6f54ed33908fb1bb6dfebaa0031c67279ed0
jafar fuad <jata1518@gmail.com>
2025-06-14 04:43:25 +0300

    Add User and Roles Entities

---
+++ src/TravelAndAccommodationBookingPlatform.Core/Entities/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Core.Entities
+{
+    public class Role
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public ICollection<User> Users { get; set; } = new List<User>();
+    }
+}
