506ea6d9f2b846223d4a14556fce353f5fe8d333
jafar fuad <jata1518@gmail.com>
2025-06-15 17:05:47 +0300

    Add IRepository interface Generic repository for basic data access

---
+++ src/TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+{
+    interface IRepository<T> where T : class
+    {
+        Task<City?> GetByIdAsync(Guid id);
+        Task<City> AddAsync(T entity);
+        Task RemoveAsync(Guid id);
+        Task UpdateAsync(T entity);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    }
+}
