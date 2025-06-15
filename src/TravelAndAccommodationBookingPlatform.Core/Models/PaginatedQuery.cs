160af7852061cf1bc027137f8ee74b4681b93a82
jafar fuad <jata1518@gmail.com>
2025-06-15 15:24:10 +0300

    Add PaginatedQuery record for handling paginated queries with filtering, sorting, and pagination options

---
+++ src/TravelAndAccommodationBookingPlatform.Core/Models/PaginatedQuery.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Models
+{
+    public record PaginatedQuery<TEntity>(
+        Expression<Func<TEntity, bool>> FilterExpression,
+        string? SortByColumn,
+        int PageNumber,
+        int PageSize,
+        OrderDirection SortDirection = OrderDirection.Ascending
+    );
+
+}
