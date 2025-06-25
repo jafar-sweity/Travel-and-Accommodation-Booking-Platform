5c26ed77d18fa02c5e9eee90ac1a8637abaf702c
jafar fuad <jata1518@gmail.com>
2025-06-25 21:08:07 +0300

    Add IDiscountRepository interface for discount-related persistence operations

---
+++ src/TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IDiscountRepository.cs
@@ -0,0 +1,10 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+{
+    interface IDiscountRepository
+    {
+        Task<PaginatedResult<Discount>> GetDiscountsAsync(PaginatedQuery<Discount> query);
+    }
+}
