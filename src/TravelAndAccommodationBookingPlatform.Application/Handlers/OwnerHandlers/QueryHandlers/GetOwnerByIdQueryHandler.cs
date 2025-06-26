8cbbe791344692c33aa3d9a220b9df4128f2aca7
jafar fuad <jata1518@gmail.com>
2025-06-26 20:34:47 +0300

    Add GetOwnerById feature with query, handler, and exception handling for retrieving owner details by ID

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Handlers/OwnerHandlers/QueryHandlers/GetOwnerByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.OwnerDtos;
+using TravelAndAccommodationBookingPlatform.Application.Queries.OwnerQueries;
+using TravelAndAccommodationBookingPlatform.Core.DomainMessages;
+using TravelAndAccommodationBookingPlatform.Core.Exceptions;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+
+namespace TravelAndAccommodationBookingPlatform.Application.Handlers.OwnerHandlers.QueryHandlers
+{
+    class GetOwnerByIdQueryHandler : IRequestHandler<GetOwnerByIdQuery, OwnerResponseDto>
+    {
+        private readonly IOwnerRepository _ownerRepository;
+        private readonly IMapper _mapper;
+        public GetOwnerByIdQueryHandler(
+            IOwnerRepository ownerRepository,
+            IMapper mapper)
+        {
+            _ownerRepository = ownerRepository;
+            _mapper = mapper;
+        }
+        public async Task<OwnerResponseDto> Handle(GetOwnerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var owner = await _ownerRepository.GetByIdAsync(request.Id) ?? throw new NotFoundException(OwnerMessages.OwnerNotFound);
+
+            return _mapper.Map<OwnerResponseDto>(owner);
+        }
+    }
+}
