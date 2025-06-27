f6710db98e89fc79aa74a683842dab95cd125e3b
jafar fuad <jata1518@gmail.com>
2025-06-27 19:52:11 +0300

    Add IPasswordHashService and Implement PasswordHashService

---
+++ src/TravelAndAccommodationBookingPlatform.Core/Interfaces/Services/Authentication/PasswordHashService.cs
@@ -0,0 +1,18 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Interfaces.Auth;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Services.Authentication
+{
+
+    public class PasswordHashService : IPasswordHashService
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
