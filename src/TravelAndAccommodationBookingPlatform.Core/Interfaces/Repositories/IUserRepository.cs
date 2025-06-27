eac2314aa01efafcab173f510f346cfa7ccbfe1a
jafar fuad <jata1518@gmail.com>
2025-06-27 16:56:13 +0300

    Add IRepository for user-related operation

---
+++ src/TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+{
+    interface IUserRepository : IRepository<User>
+    {
+        Task<User?> AuthenticateUserAsync(string email, string password);
+        Task<bool> UserExistsByEmailAsync(string email);
+
+    }
+}
