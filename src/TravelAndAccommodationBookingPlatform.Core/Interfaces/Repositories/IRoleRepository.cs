3b31ebbe34dec5ea6684d12eba4df6ffde2af567
jafar fuad <jata1518@gmail.com>
2025-06-27 20:15:08 +0300

    Add IRoleRepository for Role opertation

---
+++ src/TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IRoleRepository.cs
@@ -0,0 +1,9 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+{
+    public interface IRoleRepository : IRepository<Role>
+    {
+        Task<Role?> GetRoleByNameAsync(string name);
+    }
+}
