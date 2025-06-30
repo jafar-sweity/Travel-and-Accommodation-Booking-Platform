e245823a3d9ded96ba916116e5cc1deb2eea7f6d
jafar fuad <jata1518@gmail.com>
2025-06-30 23:39:30 +0300

    Add CityResponseDto and Implement CreatCityHandler

---
+++ src/TravelAndAccommodationBookingPlatform.Application/DTOs/CityDtos/CityResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Application.DTOs.CityDtos
+{
+    class CityResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string PostOffice { get; set; } = string.Empty;
+    }
+}
