cd24a8ce7032e09e1dab6867e01078b339ea3a9b
jafar fuad <jata1518@gmail.com>
2025-06-30 23:38:26 +0300

    Add  CreateCityCommand

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Commands/CityCommands/CreateCityCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.CityDtos;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Commands.CityCommands
+{
+    class CreateCityCommand : IRequest<CityResponseDto>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string PostOffice { get; set; } = string.Empty;
+    }
+}
