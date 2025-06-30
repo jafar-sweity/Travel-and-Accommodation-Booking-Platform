cde18b7ea9600c4b8e05f15ea8d4c92c31bd791b
jafar fuad <jata1518@gmail.com>
2025-07-01 00:01:59 +0300

    Add UpdateCity handler and command with automapper

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Commands/CityCommands/UpdateCityCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Commands.CityCommands
+{
+    class UpdateCityCommand : IRequest
+    {
+        public Guid CityId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string PostOffice { get; set; } = string.Empty;
+    }
+}
