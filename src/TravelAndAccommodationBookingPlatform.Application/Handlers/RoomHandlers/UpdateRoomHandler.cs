a476c6dfca90372ecbf104688e6c39f457caa016
jafar fuad <jata1518@gmail.com>
2025-07-01 22:22:21 +0300

    Implement UpdateRoom Handler

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Handlers/RoomHandlers/UpdateRoomHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.Commands.RoomCommands;
+using TravelAndAccommodationBookingPlatform.Core.Constants.DomainMessages;
+using TravelAndAccommodationBookingPlatform.Core.Exceptions;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.UnitOfWork;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Handlers.RoomHandlers
+{
+    class UpdateRoomHandler : IRequestHandler<UpdateRoomCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRoomRepository _roomRepository;
+        private readonly IMapper _mapper;
+        private readonly IRoomClassRepository _roomClassRepository;
+
+        public UpdateRoomCommandHandler(
+            IUnitOfWork unitOfWork,
+            IRoomRepository roomRepository,
+            IMapper mapper,
+            IRoomClassRepository roomClassRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _roomRepository = roomRepository;
+            _mapper = mapper;
+            _roomClassRepository = roomClassRepository;
+        }
+
+        public async Task Handle(UpdateRoomCommand request, CancellationToken cancellationToken)
+        {
+            var roomClassExists = await _roomClassRepository.ExistsByPredicateAsync(rc => rc.Id == request.RoomClassId);
+
+            if (!roomClassExists)
+            {
+                throw new KeyNotFoundException("Room class not found.");
+            }
+
+            var room = await _roomRepository.GetByIdAsync(request.RoomId) ??
+                throw new NotFoundException(RoomMessages.RoomNotFound);
+
+            _mapper.Map(request, room);
+
+            await _roomRepository.UpdateAsync(room);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
