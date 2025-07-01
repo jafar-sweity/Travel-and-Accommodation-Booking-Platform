08ca10af25ce60f67b552c65c6789fe6d04d674a
jafar fuad <jata1518@gmail.com>
2025-07-01 17:06:40 +0300

    Add IcurrentUserservice interface

---
+++ src/TravelAndAccommodationBookingPlatform.Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,22 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Services.Authentication;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public string GetUserEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid GetUserId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUserRole()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
