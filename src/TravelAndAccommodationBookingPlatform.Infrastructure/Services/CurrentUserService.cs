2b2f699d4b7f998b3eb709cb3899022b4bbd0f02
jafar fuad <jata1518@gmail.com>
2025-07-01 17:14:06 +0300

    Implement CurrentUserService to check the user authorization

--- src/TravelAndAccommodationBookingPlatform.Infrastructure/Services/CurrentUserService.cs
+++ src/TravelAndAccommodationBookingPlatform.Infrastructure/Services/CurrentUserService.cs
@@ -1,22 +1,47 @@
-﻿using TravelAndAccommodationBookingPlatform.Core.Services.Authentication;
+﻿using Microsoft.AspNetCore.Http;
+using TravelAndAccommodationBookingPlatform.Core.Constants.DomainMessages;
+using TravelAndAccommodationBookingPlatform.Core.Exceptions;
+using TravelAndAccommodationBookingPlatform.Core.Services.Authentication;
 
 namespace TravelAndAccommodationBookingPlatform.Infrastructure.Services
 {
     public class CurrentUserService : ICurrentUserService
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public string GetUserEmail()
         {
-            throw new NotImplementedException();
+            var emailClaim = _httpContextAccessor.HttpContext?.User?.FindFirst("email")?.Value;
+
+            if (string.IsNullOrEmpty(emailClaim))
+                throw new UnauthorizedException(UserMessages.UserNotAuthenticated);
+
+            return emailClaim;
         }
 
         public Guid GetUserId()
         {
-            throw new NotImplementedException();
+            var userIdClaim = _httpContextAccessor.HttpContext?.User?.FindFirst("sub")?.Value;
+
+            if (string.IsNullOrEmpty(userIdClaim) || !Guid.TryParse(userIdClaim, out var userId))
+                throw new UnauthorizedException(UserMessages.UserNotAuthenticated);
+
+            return userId;
         }
 
         public string GetUserRole()
         {
-            throw new NotImplementedException();
+            var roleClaim = _httpContextAccessor.HttpContext?.User?.FindFirst("role")?.Value;
+
+            if (string.IsNullOrEmpty(roleClaim))
+                throw new UnauthorizedException(UserMessages.UserNotAuthenticated);
+
+            return roleClaim;
         }
     }
 }
