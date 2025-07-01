c5585845590fa095ad854c2e0be412fa769cda0b
jafar fuad <jata1518@gmail.com>
2025-07-01 17:17:39 +0300

    Add  GetBookingById Query

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Queries/BookingQueries/GetBookingByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.BookingDtos;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Queries.BookingQueries
+{
+    class GetBookingByIdQuery : IRequest<BookingResponseDTO>
+    {
+        public Guid BookingId { get; set; }
+    }
+}
