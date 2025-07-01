0e41390816d18e4e51d60551fb8922ee6828d58e
jafar fuad <jata1518@gmail.com>
2025-07-01 16:47:43 +0300

    Add Booking Response Dto

---
+++ src/TravelAndAccommodationBookingPlatform.Application/DTOs/BookingDtos/BookingResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Application.DTOs.BookingDtos
+{
+    class BookingResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string HotelName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string? GuestRemarks { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime BookingDate { get; set; }
+
+    }
+}
