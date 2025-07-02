ae875cd845562bde374a6d0d276845269a38d82c
jafar fuad <jata1518@gmail.com>
2025-07-02 21:18:31 +0300

    Add GuestRoomsByClassId & GetRoomsManagement Queries

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Queries/RoomQueries/GuestRoomsByClassIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.RoomDtos;
+using TravelAndAccommodationBookingPlatform.Core.Models;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Queries.RoomQueries
+{
+    class GuestRoomsByClassIdQuery : IRequest<PaginatedResult<RoomGuestResponseDto>>
+    {
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+        public Guid RoomClassId { get; init; }
+        public DateOnly CheckInDate { get; init; }
+        public DateOnly CheckOutDate { get; init; }
+    }
+}
