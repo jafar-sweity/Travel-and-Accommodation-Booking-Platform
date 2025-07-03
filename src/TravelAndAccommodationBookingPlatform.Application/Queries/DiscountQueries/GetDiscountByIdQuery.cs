7f373d6eae502fe9ff6b118728e804585a595a6d
jafar fuad <jata1518@gmail.com>
2025-07-03 16:50:58 +0300

    Implement GetDiscountByIdQuery Handler & Add GetDiscountByIdQuery

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Queries/DiscountQueries/GetDiscountByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.DiscountDtos;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Queries.DiscountQueries
+{
+    class GetDiscountByIdQuery : IRequest<DiscountResponseDto>
+    {
+        public Guid DiscountId { get; init; }
+        public Guid RoomClassId { get; init; }
+    }
+}
