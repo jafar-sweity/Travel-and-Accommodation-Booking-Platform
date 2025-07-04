42b78d4f2691852e38c70d8f99153dc1631fd565
jafar fuad <jata1518@gmail.com>
2025-07-04 16:08:29 +0300

    Add GetHotelGuestByIdQueryHandler implementation with AutoMapper and exception handling for retrieving hotel details for guests

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Handlers/HotelHandlers/GetHotelGuestByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.HotelDtos;
+using TravelAndAccommodationBookingPlatform.Application.Queries.HotelQueries;
+using TravelAndAccommodationBookingPlatform.Core.Constants.DomainMessages;
+using TravelAndAccommodationBookingPlatform.Core.Exceptions;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Handlers.HotelHandlers
+{
+    public class GetHotelGuestByIdQueryHandler : IRequestHandler<GetHotelGuestByIdQuery, HotelGuestResponseDto>
+    {
+        private readonly IHotelRepository _hotelRepository;
+        private readonly IMapper _mapper;
+
+        public GetHotelGuestByIdQueryHandler(IHotelRepository hotelRepository, IMapper mapper)
+        {
+            _hotelRepository = hotelRepository;
+            _mapper = mapper;
+        }
+        public async Task<HotelGuestResponseDto> Handle(GetHotelGuestByIdQuery request, CancellationToken cancellationToken)
+        {
+            var hotel = await _hotelRepository.GetByIdAsync(request.HotelId);
+            return hotel == null ? throw new NotFoundException(HotelMessages.HotelNotFound) : _mapper.Map<HotelGuestResponseDto>(hotel);
+        }
+    }
+}
--- src/TravelAndAccommodationBookingPlatform.Application/Profiles/HotelProfile.cs
+++ src/TravelAndAccommodationBookingPlatform.Application/Profiles/HotelProfile.cs
@@ -13,6 +13,11 @@ namespace TravelAndAccommodationBookingPlatform.Application.Profiles
             CreateMap<CreateHotelCommand, Hotel>();
             CreateMap<UpdateHotelCommand, Hotel>();
             CreateMap<HotelManagementDto, HotelManagementResponseDto>().ForMember(dst => dst.Owner, options => options.MapFrom(src => src.Owner));
+            CreateMap<Hotel, HotelGuestResponseDto>()
+            .ForMember(dest => dest.CountryName, opt => opt.MapFrom(src => src.City.Country))
+            .ForMember(dest => dest.CityName, opt => opt.MapFrom(src => src.City.Name))
+            .ForMember(dest => dest.Thumbnail, opt => opt.MapFrom(src => src.SmallPreview != null ? src.SmallPreview.Url : null))
+            .ForMember(dest => dest.Gallery, opt => opt.MapFrom(src => src.FullView.Select(image => image.Url)));
         }
     }
 }
