bf74c10fa46d61ec46cdc3fadf0df77e07fe6224
jafar fuad <jata1518@gmail.com>
2025-07-04 17:22:48 +0300

    Implement hotel search handler with filters, room availability check, and pagination

--- src/TravelAndAccommodationBookingPlatform.Core/Models/HotelSearchDto.cs
+++ src/TravelAndAccommodationBookingPlatform.Core/Models/HotelSearchDto.cs
@@ -9,5 +9,6 @@
         public string? CityName { get; init; }
         public double ReviewsRating { get; set; }
         public decimal NightlyRate { get; set; }
-        public string? Description { get; set; }    }
+        public string? Description { get; set; }
+    }
 }
