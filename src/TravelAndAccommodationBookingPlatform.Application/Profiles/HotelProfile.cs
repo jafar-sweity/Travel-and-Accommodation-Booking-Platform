81f025c2643835eaef8b39cd84d5570a8039ea31
jafar fuad <jata1518@gmail.com>
2025-07-04 15:53:36 +0300

    Implement CreateHotelCommandHandler & Add HotelProfile for AutoMapping

---
+++ src/TravelAndAccommodationBookingPlatform.Application/Profiles/HotelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.HotelCommands;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Profiles
+{
+    class HotelProfile : Profile
+    {
+        public HotelProfile()
+        {
+            CreateMap<CreateHotelCommand, Hotel>();
+        }
+    }
+}
