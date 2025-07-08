ac5f4acb0954d8e3fab1e8413fd160c561505465
jafar fuad <jata1518@gmail.com>
2025-07-08 13:33:23 +0300

    Implement CreateBookingCommand & Add BookingProfile for AutoMapper

---
+++ src/TravelAndAccommodationBookingPlatform.WebAPI/Mapping/BookingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Queries.BookingQueries;
+using TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Bookings;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Mapping
+{
+    public class BookingProfile : Profile
+    {
+        public BookingProfile()
+        {
+            CreateMap<BookingCreationRequestDto, CreateBookingC>();
+            CreateMap<GetBookingsRequestDto, GetBookingQuery>()
+                .ForMember(
+                    dst => dst.OrderDirection,
+                    opt => opt.MapFrom(src => src.OrderDirection));
+        }
+    }
+}
