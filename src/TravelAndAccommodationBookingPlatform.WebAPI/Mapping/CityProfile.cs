55806a1566664fa297d282b1cc5dc7cf82442efe
jafar fuad <jata1518@gmail.com>
2025-07-08 12:55:34 +0300

    Add GetCitiesManagementQuery

---
+++ src/TravelAndAccommodationBookingPlatform.WebAPI/Mapping/CityProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.CityCommands;
+using TravelAndAccommodationBookingPlatform.Application.Queries.CityQueries;
+using TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Cities;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Mapping
+{
+    public class CityProfile : Profile
+    {
+        public CityProfile()
+        {
+            CreateMap<GetTrendingCitiesRequestDto, GetTrendingCitiesQuery>();
+            CreateMap<CityCreationRequestDto, CreateCityCommand>();
+            CreateMap<CityUpdateRequestDto, UpdateCityCommand>();
+            CreateMap<GetCitiesRequestDto, GetCitiesManagementQuery>()
+                .ForMember(
+                    dst => dst.OrderDirection,
+                    opt => opt.MapFrom(src => src.OrderDirection));
+        }
+    }
+}
