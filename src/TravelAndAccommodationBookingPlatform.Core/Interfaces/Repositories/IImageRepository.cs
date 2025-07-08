0f48f2f583ad82f789108a46165df90b40a1c54f
jafar fuad <jata1518@gmail.com>
2025-07-08 10:59:09 +0300

    Add implement IImageRepository

--- src/TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IImageRepository.cs
+++ src/TravelAndAccommodationBookingPlatform.Core/Interfaces/Repositories/IImageRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
+﻿using Microsoft.AspNetCore.Http;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces.Repositories
 {
     public interface IImageRepository
     {
+        Task<Image> UploadImageAsync(IFormFile image, Guid entityId, string type);
+
+        Task RemoveImageAsync(Guid entityId, string type);
     }
 }
