deee7f02be2e4f42a8754e2f72eff4fa40f3a8de
jafar fuad <jata1518@gmail.com>
2025-07-11 22:28:05 +0300

    Add GetHotelFeaturedDealsQueryHandler & update HotelProfile

--- src/TravelAndAccommodationBookingPlatform.Application/Profiles/HotelProfile.cs
+++ src/TravelAndAccommodationBookingPlatform.Application/Profiles/HotelProfile.cs
@@ -24,6 +24,21 @@ namespace TravelAndAccommodationBookingPlatform.Application.Profiles
                .ForMember(dest => dest.SmallPreview, opt => opt.MapFrom(src => src.ThumbnailImage != null ? src.ThumbnailImage.ToString() : null));
             CreateMap<PaginatedResult<HotelSearchDto>, PaginatedResult<HotelSearchResultResponseDto>>().ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
             CreateMap<PaginatedResult<HotelManagementDto>, PaginatedResult<HotelManagementResponseDto>>().ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+
+            CreateMap<RoomClass, HotelFeaturedDealResponseDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Hotel.Name))
+            .ForMember(dest => dest.RoomClassName, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.CityName, opt => opt.MapFrom(src => src.Hotel.City.Name))
+            .ForMember(dest => dest.Geolocation, opt => opt.MapFrom(src => src.Hotel.Geolocation))
+            .ForMember(dest => dest.StarRating, opt => opt.MapFrom(src => src.Hotel.StarRating))
+            .ForMember(dest => dest.NightlyRate, opt => opt.MapFrom(src => src.NightlyRate))
+            .ForMember(dest => dest.DiscountPercentage, opt => opt.MapFrom(src => src.Discounts.First().Percentage))
+            .ForMember(dest => dest.DiscountStartDate, opt => opt.MapFrom(src => src.Discounts.First().StartDate))
+            .ForMember(dest => dest.DiscountEndDate, opt => opt.MapFrom(src => src.Discounts.First().EndDate))
+            .ForMember(dest => dest.ReviewsRating, opt => opt.MapFrom(src => src.Hotel.ReviewsRating))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Hotel.BriefDescription))
+            .ForMember(dest => dest.Thumbnail, opt => opt.MapFrom(src => src.Hotel.SmallPreview == null ? null : src.Hotel.SmallPreview.Url));
         }
     }
 }
