4d98c5c16a96753b5017bc35d79105f3cf88aae8
jafar fuad <jata1518@gmail.com>
2025-07-15 09:19:07 +0300

    Add ImageCreationValidator Using FluentValidation

---
+++ src/TravelAndAccommodationBookingPlatform.WebAPI/Validators/Image/ImageCreationRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TravelAndAccommodationBookingPlatform.WebAPI.DTOs.Images;
+
+namespace TravelAndAccommodationBookingPlatform.WebAPI.Validators.Images
+{
+    public class ImageCreationRequestValidator : AbstractValidator<ImageCreationRequestDto>
+    {
+
+        public ImageCreationRequestValidator()
+        {
+            RuleFor(x => x.Image)
+                .NotNull().WithMessage("Image file is required.")
+                .Must(image => image.Length > 0).WithMessage("Image file must not be empty.");
+        }
+    }
+}
